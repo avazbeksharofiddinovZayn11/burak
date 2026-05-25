b5a64008c75a6cf9c76949f9ec2a905756e486c6
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-05-26 01:49:37 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// __dirname ni yasash
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+export default app;
