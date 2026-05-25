911303e501453b171298cbb17382c58f494d8bdc
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-05-26 02:10:11 +0900

    feat: M task train

--- train.py
+++ train.py
@@ -1,15 +1,32 @@
+# M task
+def getSquareNumbers(arr):
+    result = []
+
+    for num in arr:
+        obj = {
+            "number": num,
+            "square": num * num
+        }
+
+        result.append(obj)
+
+    return result
+
+
+print(getSquareNumbers([8, 5, 9, 3,]))
+
+
 # L task
 
+# def reverseSentence(text):
+#     words = text.split()
 
-def reverseSentence(text):
-    words = text.split()
+#     reversed_words = []
 
-    reversed_words = []
+#     for word in words:
+#         reversed_words.append(word[::-1])
 
-    for word in words:
-        reversed_words.append(word[::-1])
-
-    return " ".join(reversed_words)
+#     return " ".join(reversed_words)
 
 
-print(reverseSentence("we like coding!"))
+# print(reverseSentence("we like coding!"))
