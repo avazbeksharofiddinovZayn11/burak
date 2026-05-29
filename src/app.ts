aefef97f013b5609785992c83bf27305cfe0fea0
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-05-29 23:22:34 +0900

    feat: create and membercontroller

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import router from "./views/router.js";
 
 // __dirname ni yasash
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// 1 Extrance
 const app = express();
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// 2 Sessions
 
+// 3 Views
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// Routers
+app.use("/", router)  // Middleware Design Pettern
+
+
 
 export default app;
