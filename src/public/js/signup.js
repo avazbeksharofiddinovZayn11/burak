1c02e64f50a9a49d62c7a830a1205b1db53b9c83
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-06-20 07:11:29 +0900

    feat: signup develop files logic

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,33 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+
+        filename = $(this)[0].files[0].name;
+      }
+
+      $(this).siblings(".upload-name").val(filename);
+    }
+  })
+  })();
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -21,6 +48,14 @@ function validateSignupForm() {
 
   if (memberPassword !== confirmPassword) {
     alert(" Password differs, please check!");
+    return false;
+  }
+
+  const memberImage = $(".meber-image").get(0).file[0].files[0].name
+  ? $(".member-image").get(0).files[0].name
+  : null;
+  if(!memberImage) {
+    alert("Please insert restaurant image");
     return false;
   }
 }
