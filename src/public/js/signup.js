0f25099d65d5cee999d73c4c4aca15f2aefd019a
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-06-20 18:39:20 +0900

    fix: modify signup.js

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,6 @@
 console.log("Signup frontend javascript file");
 
-(function () {
+$(function () {
   const fileTarget = $(".file-box .upload-hidden");
   let filename;
 
@@ -9,25 +9,21 @@ console.log("Signup frontend javascript file");
       const uploadFile = $(this)[0].files[0];
       const fileType = uploadFile["type"];
       const validImageType = ["image/jpg", "image/jpeg", "image/png"];
-
       if (!validImageType.includes(fileType)) {
-        alert("Please insert only jpeg, jpg and png!");
+        alert("Please inseart only jpeg, jpg and png!");
       } else {
         if (uploadFile) {
           console.log(URL.createObjectURL(uploadFile));
-
           $(".upload-img-frame")
             .attr("src", URL.createObjectURL(uploadFile))
             .addClass("success");
         }
-
         filename = $(this)[0].files[0].name;
       }
-
       $(this).siblings(".upload-name").val(filename);
     }
-  })
-  })();
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -35,27 +31,27 @@ function validateSignupForm() {
   const memberPassword = $(".member-password").val();
   const confirmPassword = $(".confirm-password").val();
 
-  if(
-    memberNick === "" || 
-    memberPhone === ""  || 
-    memberPassword === "" || 
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
     confirmPassword === ""
-
   ) {
-    alert(" Please insert all required inputs!");
+    alert("Please insert all required inputs!");
     return false;
   }
 
   if (memberPassword !== confirmPassword) {
-    alert(" Password differs, please check!");
+    alert("Password differs, please check!");
     return false;
   }
 
-  const memberImage = $(".meber-image").get(0).file[0].files[0].name
-  ? $(".member-image").get(0).files[0].name
-  : null;
-  if(!memberImage) {
-    alert("Please insert restaurant image");
+  const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
     return false;
   }
+  return true;
 }
