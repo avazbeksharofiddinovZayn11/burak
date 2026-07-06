eaaefae7a5809d23856227954b7208ab729f51af
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-07-07 02:39:37 +0900

    feat: develop getTopUser rest Api

--- src/models/Members.service.ts
+++ src/models/Members.service.ts
@@ -81,7 +81,17 @@ class Membercervice {
       .findByIdAndUpdate({ _id: memberId }, input, { new: true })
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
-    
+
+    return result;
+  }
+
+  public async getTopUsers(): Promise<Member[]> {
+    const result = await this.memberModel
+      .find({ memberStatus: MemberStatus.ACTIVE, memberPoints: { $gte: -1 } })
+      .sort({ memberPoints: -1 })
+      .limit(4)
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
     return result;
   }
 
